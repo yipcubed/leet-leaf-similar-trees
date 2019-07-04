c93a2b1bc2f82394c6f5a76f148706b2630c0e6f
Yip Cubed <yipcubed@gmail.com>
2019-07-04 09:05:25 -1000

    starting

--- main.cpp
+++ main.cpp
@@ -60,17 +60,32 @@ public:
         inorder(root2, right);
         return left == right;
     }
+
+/*
+ * When inorder is already defined
+
+    bool leafSimilar(TreeNode *root1, TreeNode *root2) {
+        if (!root1 && !root2) return true;
+        if (!root1 || !root2) return false;
+        vector<int> r1leaves;
+        vector<int> r2leaves;
+        auto r1lam = [&r1leaves](int val) { r1leaves.push_back(val); };
+        auto r2lam = [&r2leaves](int val) { r2leaves.push_back(val); };
+        root1->inorder(r1lam);
+        root2->inorder(r2lam);
+        return r1leaves == r2leaves;
+    }
+
+ */
 };
 
 void test1() {
-    vector<int> v1 {3, 5, 1, 6, 2, 9, 8, -1, -1, 7, 4};
+    vector<int> v1{3, 5, 1, 6, 2, 9, 8, -1, -1, 7, 4};
     auto t1 = new TreeNode(v1, -1);
-    cout << t1 << endl;
 
-    vector<int> v2 {3, -1, 4, 8, 9, 66, 77, 88, 99, 6, 7, -1, 4, -1, 9, -1, 8};
+    vector<int> v2{3, -1, 4, 8, 9, 66, 77, 88, 99, 6, 7, -1, 4, -1, 9, -1, 8};
     auto t2 = new TreeNode(v2, -1);
-    cout << t2 << endl;
-    cout << "1 ? \n" << Solution().leafSimilar(t1, t2) << endl;
+    cout << "1 ? " << Solution().leafSimilar(t1, t2) << endl;
 
 }
 
